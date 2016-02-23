fc79ceea683a1ab7456087261079636ca5de9238
nhatvo15 <nvo001@plattsburgh.edu>
2016-02-23 07:42:31 -0500

    Update enemy.py
    Added getAtt(self) and getDef(self)

--- enemy.py
+++ enemy.py
@@ -4,61 +4,88 @@
 
 #Buster Baker-Porazinski 
 
-class enemy(object):
-	
-	def __init__(self, punchd, kickd, grapd, puncho, kicko, grapo, hp, speed, name, appid): 
+import random
+class Enemy(object):
+    def __init__(self, punchd, kickd, grapd, puncho, kicko, grapo,
+                 hp, speed, name, appid): 
+            """Defense"""
+            self.punchd = punchd
+            self.kickd = kickd
+            self.grapd = grapd
 
-		self.punchd = punchd
-		self.kickd = kickd
-		self.grapd = grapd
+            """Offense"""
+            self.puncho = puncho
+            self.kicko = kicko
+            self.grapo = grapo 
 
-		self.puncho = puncho
-		self.kicko = kicko
-		self.grapo = grapo 
+            """Related stat"""
+            self.hp = hp
+            self.speed = speed
+            self.name = name
+            self.appid = appid
+    
+    def getPunchd(self):
+        return self.punchd
 
-		self.hp = hp
-		self.speed = speed
-		self.name = name
-		self.appid = appid
+    def getKickd(self):
+        return self.kickd
 
-	def getPunchd(self):
-		return self.punchd
+    def getGrapd(self):
+        return self.grapd
 
-	def getKickd(self):
-		return self.kickd
+    """getDefends return randomly one of the three defense alements"""
+    def getDef(self):
+        num = random.randint(0, 2)
+        methid, damage = "", 0
+        if num == 0:
+            method = "hammer"
+            damage = self.punchd
+        elif num == 1:
+            method = "sword"
+            damage = self.kickd
+        else:
+            method = "shield"
+            damage = self.grapd
+        return method, damage
 
-	def getGrapd(self):
-		return self.grapd
+    def getPuncho(self):
+        return self.puncho
 
+    def getKicko(self):
+        return self.kicko
 
+    def getGrapo(self):
+        return self.grapo
 
-	def getPuncho(self):
-		return self.puncho
+    """getOffends return randomly one of the three offense elements"""
+    def getAtt(self):
+        num = random.randint(0, 2)
+        method, damage = "", 0
+        if num == 0:
+            method = "hammer"
+            damage = self.puncho
+        elif num == 1:
+            method = "sword"
+            damage = self.kicko
+        else:
+            method = "shield"
+            damage = self.grapo
+        return method, damage
 
-	def getKicko(self):
-		return self.kicko
+    def getHp(self):
+            return self.hp
+        
+    def setHp(self, val):
+        self.hp = val
 
-	def getGrapo(self):
-		return self.grapo
+    def getSpeed(self):
+            return self.speed
 
+    def getName(self):
+            return self.name
 
+    def getAppid(self):
+            return self.appid
 
-	def getHp(self):
-		return self.hp
-
-	def getSpeed(self):
-		return self.speed
-
-	def getName(self):
-		return self.name
-
-	def getAppid(self):
-		return self.appid
-
-
-
-	def __str__ (self):
-		return "%s is an enemy." % (self.name)
-
-
-
+    def __str__ (self):
+            return "%s is an enemy." % (self.name)
